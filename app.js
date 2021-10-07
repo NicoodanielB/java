480d5388b854e90f53abfcb81b4c4cbe385afeea
Nicolas Bernstein <nicoo.bernstein@gmail.com>
2021-10-06 23:56:36 -0300

    -

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeros = [];
+
 class Crearelemento{
     constructor(numerodeorden, especificaciones, reparacion, precio){
         numerodeorden = numerodeorden;
@@ -8,11 +10,14 @@ class Crearelemento{
 }
 
 let numerodeorden = 0;
-function generarorden(){
+function generarelemento(){
 for (i = numerodeorden; i++;) {
     let especificaciones = prompt("Ingrese las especificaciones de la computadora");
     let reparacion = prompt("Ingrese los daños a reparar");
     let precio = Number(prompt("Ingrese el precio"));
+    Crearelemento(numerodeorden, especificaciones, reparacion, precio);
+    numeros.push(Crearelemento);
+    console.log(numerodeorden);
 break;
 }
 }
