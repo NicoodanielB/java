eb5338a6eee4250d2474eb3224155e6df3b727a5
Nicolas Bernstein <nicoo.bernstein@gmail.com>
2021-10-06 23:52:05 -0300

    -

---
+++ app.js
@@ -0,0 +1,18 @@
+class Crearelemento{
+    constructor(numerodeorden, especificaciones, reparacion, precio){
+        numerodeorden = numerodeorden;
+        especificaciones = especificaciones;
+        reparacion = reparacion;
+        precio = precio;
+    }
+}
+
+let numerodeorden = 0;
+function generarorden(){
+for (i = numerodeorden; i++;) {
+    let especificaciones = prompt("Ingrese las especificaciones de la computadora");
+    let reparacion = prompt("Ingrese los daños a reparar");
+    let precio = Number(prompt("Ingrese el precio"));
+break;
+}
+}
